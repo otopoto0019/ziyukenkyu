d1d456d5778a3f9d0352742695a52e69c2996a9e
Mikami Shuto <115867603+otopoto0019@users.noreply.github.com>
2024-01-16 22:18:24 +0900

    Update ApproximateCircleAreaCalculator.java

--- src/ApproximateCircleAreaCalculator.java
+++ src/ApproximateCircleAreaCalculator.java
@@ -7,8 +7,8 @@ import java.util.function.BiFunction;
 
 
 public class ApproximateCircleAreaCalculator {
+    //xは,radiumは計算したい円の半径
     static BiFunction<Fraction, Fraction, SquareRoot> calculateHeight = (x, radium) -> new SquareRoot(Fraction.subtractFractions(Fraction.powerFraction(radium,2),Fraction.powerFraction(x,2)));
-    static BiFunction<Fraction, SquareRoot, SquareRoot> calculateSectionalSquare = SquareRoot::multiplySquareRoot;
 
     //xは区分間隔,radiumは計算したい円の半径
     static BiFunction<Fraction, Fraction, Double> sumTotalArea = (x, radium) -> {
@@ -18,12 +18,12 @@ public class ApproximateCircleAreaCalculator {
         double result;
 
         while (!Fraction.compareFractionSize(bottom, radium)){
-            if (Math.abs(calculateSectionalSquare.apply(x, calculateHeight.apply(x, radium)).getRoot_number().getDenominator()) >= Math.pow(2, 31) -1 ||
-                    Math.abs(calculateSectionalSquare.apply(x, calculateHeight.apply(x, radium)).getRoot_number().getNumerator()) >= Math.pow(2, 31) -1) {
+            if (Math.abs(multiplySquareRoot(x, calculateHeight.apply(x, radium)).getRoot_number().getDenominator()) >= Math.pow(2, 31) -1 ||
+                    Math.abs(multiplySquareRoot(x, calculateHeight.apply(x, radium)).getRoot_number().getNumerator()) >= Math.pow(2, 31) -1) {
                 System.out.println("半径が大きすぎるか区分間隔が小さすぎます。");
                 break;
             }
-            sectionalSquares.add(calculateSectionalSquare.apply(x, calculateHeight.apply(bottom, radium)).rationalizeSquareRoot());
+            sectionalSquares.add(multiplySquareRoot(x, calculateHeight.apply(bottom, radium)).rationalizeSquareRoot());
             bottom.addFraction(x);
         }
         result = sectionalSquares.stream()
